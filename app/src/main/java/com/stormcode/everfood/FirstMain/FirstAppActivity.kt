077d810c99db30ad9a5573cdd230d86ba83e351b
onix3 <onix3891@gmail.com>
2024-09-14 08:00:05 -0500

    verificacion users json

--- app/src/main/java/com/stormcode/everfood/FirstMain/FirstAppActivity.kt
+++ app/src/main/java/com/stormcode/everfood/FirstMain/FirstAppActivity.kt
@@ -2,23 +2,40 @@ package com.stormcode.everfood.FirstMain
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.stormcode.everfood.FirstMain.UserRepository
 import com.stormcode.everfood.R
 
 
 class FirstAppActivity : AppCompatActivity() {
+
+    private  lateinit var userRepository : UserRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_first_app)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        userRepository = UserRepository(this)
+
+        val usernameEditText : EditText = findViewById(R.id.username)
+        val passwordEditText : EditText = findViewById(R.id.password)
+        val loginBtn : Button = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username =  usernameInput.text.toString()
+            val password =  passwordInput.text.toString()
+
+            if (userRepository.authenticateUser(username, password)) {
+
+            } else { print("usuario o contraseña incorrecta")}
+
+
+        }
         }
 
         val navigateButton: ImageButton = findViewById(R.id.back_button)
@@ -28,4 +45,3 @@ class FirstAppActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
-}
---
+++ app/src/main/java/com/stormcode/everfood/FirstMain/UserRepository.kt
@@ -0,0 +1,27 @@
+package import com.stormcode.everfood.FirstMain
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.InputStreamReader
+
+data class User(
+    val username: String,
+    val password: String
+)
+
+class UserRepository(private val context: Context) {
+
+    private fun loadUsersFromAssets(): List<User> {
+        val inputStream = context.assets.open("users.json")
+        val reader = InputStreamReader(inputStream)
+        val gson = Gson()
+        val userType = object : TypeToken<List<User>>() {}.type
+        return gson.fromJson(reader, userType)
+    }
+
+    fun authenticateUser(username: String, password: String): Boolean {
+        val users = loadUsersFromAssets()
+        return users.any { it.username == username && it.password == password }
+    }
+}
